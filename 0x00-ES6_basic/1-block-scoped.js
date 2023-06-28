dd18bfe92f6ae46b9f7617caa365fba6d0cf6e10
Client <client@Clients-MacBook-Pro.local>
2023-06-28 07:06:35 +0100

    my commit

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;  // Changed 'var' to 'let'
-    let task2 = true;  // Changed 'var' to 'let'
+    const task = false;  // Changed 'var' to 'let'
+    const task2 = true;  // Changed 'var' to 'let'
   
     if (trueOrFalse) {
-      let task = true;  // Changed 'var' to 'let'
-      let task2 = false;  // Changed 'var' to 'let'
+      const task = true;  // Changed 'var' to 'let'
+      const task2 = false;  // Changed 'var' to 'let'
     }
   
     return [task, task2];
