a092bacc021afecc399ecfa051615bb4e7388e65
Client <client@Clients-MacBook-Pro.local>
2023-06-29 17:22:35 +0100

    my commit

---
+++ 0x01-ES6_promise/__tests__/0-promise.test.js
@@ -0,0 +1,9 @@
+import getResponseFromAPI from "../0-promise.js";
+test("getResponseFromAPI resolves with the correct response", () => {
+    // Use the `expect` function and matchers to make assertions
+    return getResponseFromAPI().then((response) => {
+      expect(response).toEqual({
+        message: "This is the API response",
+      });
+    });
+  });
