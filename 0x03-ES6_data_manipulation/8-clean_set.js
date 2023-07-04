c6a37b71d7da7044818f23c44c932a9d84fe0f8e
Client <client@Clients-MacBook-Pro.local>
2023-07-04 20:03:37 +0100

    my commit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,16 @@
 export default function cleanSet(set, startString) {
-  const values = Array.from(set);
-  const filteredValues = values.filter((value) => value.startsWith(startString));
-  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
-  return cleanedValues.join('-');
+  const list = [];
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
+    return '';
+  }
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      list.push(item.slice(startString.length));
+    }
+  }
+  return list.join('-');
 }
